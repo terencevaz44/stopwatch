d70b653cc90189cbbee7ece11bf9146c9eff5827
terencevaz44 <129214625+terencevaz44@users.noreply.github.com>
2023-04-03 23:57:47 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,87 @@
+var hr = 0;
+var min = 0;
+var sec = 0;
+var count = 0;
+
+var timer = false;
+
+function $id(id) {
+    return document.getElementById(id);
+}
+
+function start() {
+  timer = true;
+ stopwatch();
+}
+
+function stop[() {
+  timer=false;
+  
+    hr = 0;
+    min = 0;
+    sec = 0;
+    count = 0;
+    
+    $id("hr").innerHTML = "00";
+    $id("min").innerHTML = "00";
+    $id("sec").innerHTML = "00";
+    $id("count").innerHTML = "00"; 
+  }
+  function stopwatch() {
+    if (timer == true)
+        count = count + 1;
+
+    if (count == 99) {
+        sec = sec + 1;
+        count = 0;
+    }
+     if (sec == 59) {
+        min = min + 1;
+        sec = 0;
+    }
+    if (min == 59) {
+        hr = hr + 1;
+        min = 0;
+        sec = 0;
+    }
+    
+    var hrString = hr;
+    var minString = min;
+    var secString = sec;
+    var countString = count;
+
+    if (hr < 10) {
+        hrString = "0" + hrString;
+    }
+    if (min < 10) {
+        minString = "0" + minString;
+    }
+    if (sec < 10) {
+        secString = "0" + secString;
+    }
+    if (count < 10) {
+        countString = "0" + countString;
+    }
+    
+    $id("hr").innerHTML = hrString;
+    $id("min").innerHTML = minString;
+    $id("sec").innerHTML = secString;
+    $id("count").innerHTML = countString;
+    timeoutId = setTimeout("stopwatch()", 10);
+    }
+    
+    function lap() {
+      console.log(hr,min,sec,count)
+      var laps = $id('laps');
+      laps.innerHTML += "<li>" + hr + ":" + min + ":" + sec +":" + count + "</li>";
+      }
+      
+      functiopn clearLap() {
+        $id('laps').remove();
+      }
+      
+      function getLocalTime(){
+      const d = new Date().toLocalTimeString();
+      console.log(d);
+      }
+      
